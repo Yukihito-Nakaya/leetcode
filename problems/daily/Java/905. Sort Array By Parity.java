dd9ebb3bc15009113a79afbdc89e834845c81c6f
Yukihito Nakaya <developer.nakaya@gmail.com>
2023-09-28 10:39:47 +0900

    905. Sort Array By Parity

---
+++ problems/daily/Java/905. Sort Array By Parity.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int[] res = new int[nums.length];
+        int even = 0, odd = nums.length - 1;
+
+        for(int i:nums){
+            if(i % 2 == 0){
+                res[even] = i;
+                even++;
+            }else{
+                res[odd] = i;
+                odd--;
+            }
+        }
+        return res;
+    }
+}
--- problems/leetCode 75/C++/1768. Merge Strings Alternately.cpp
+++ problems/leetCode 75/C++/1768. Merge Strings Alternately.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
