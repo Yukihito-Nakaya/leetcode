85f17bbe74f1a20c570f8934e2fc68c03e6a8246
Yukihito Nakaya <developer.nakaya@gmail.com>
2023-11-08 10:31:46 +0900

    70. Climbing Stairs

---
+++ problems/daily/Java/70. Climbing Stairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n == 1 || n == 2)return n;
+        int[] ans = new int[n];
+        ans[0]=1;
+        ans[1]=2;
+        for(int i = 2; i < n; i++){
+            ans[i]=ans[i-1] + ans[i-2];
+        }
+        return ans[n-1];
+    }
+}
