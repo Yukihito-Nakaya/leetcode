bddc88dcd78f983853a98c48b24853941a74fc7e
Yukihito Nakaya <developer.nakaya@gmail.com>
2023-11-26 15:54:57 +0900

    141. Linked List Cycle

--- problems/daily/Java/141. Linked List Cycle.java
+++ problems/daily/Java/141. Linked List Cycle.java
@@ -11,6 +11,28 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        
+       HashMap<ListNode, Boolean> map = new HashMap<>();
+       while(head != null){
+           if(map.containsKey(head)){
+               return true;
+           }
+           map.put(head,true);
+           head = head.next;
+       }
+       return false;
     }
 }
+
+//another solution
+// public class Solution {
+//     public boolean hasCycle(ListNode head) {
+//         ListNode sh = head;
+//         ListNode fh = head;
+//         while (fh != null && fh.next != null) {
+//             sh = sh.next;
+//             fh = fh.next.next;
+//             if (sh == fh) return true;
+//         }
+//         return false;
+//     }
+// }
