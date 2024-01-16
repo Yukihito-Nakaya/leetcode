ac83c5b33c3c46128d531192093ed8d1355906b6
Yukihito Nakaya <developer.nakaya@gmail.com>
2024-01-16 14:54:13 +0900

    380. Insert Delete GetRandom O(1)

---
+++ problems/daily/Java/380. Insert Delete GetRandom O(1).java
@@ -0,0 +1,39 @@
+class RandomizedSet {
+    Set<Integer> set;
+    public RandomizedSet() {
+        set = new HashSet<>();
+    }
+    
+    public boolean insert(int val) {
+        if(set.contains(val)){
+            return false;
+        }else{
+            set.add(val);
+            return true;
+        }
+    }
+    
+    public boolean remove(int val) {
+        if(set.contains(val)){
+            set.remove(val);
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+    
+    public int getRandom() {
+        List<Integer> ans = new ArrayList<>(set);
+        int i = (int)(Math.random() * set.size());
+        return ans.get(i);
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
