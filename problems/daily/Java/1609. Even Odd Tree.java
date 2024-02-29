6f9278a13a8973dbf08acbcb3b4e1c578f338175
Yukihito Nakaya <developer.nakaya@gmail.com>
2024-02-29 11:09:32 +0900

    1609. Even Odd Tree

--- problems/daily/Java/1609. Even Odd Tree.java
+++ problems/daily/Java/1609. Even Odd Tree.java
@@ -15,6 +15,36 @@
  */
 class Solution {
     public boolean isEvenOddTree(TreeNode root) {
-            
+        var q = new ArrayDeque<TreeNode>();
+        q.offer(root);
+
+        int lv = 0;
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            int prev = lv % 2 == 0 ? 0 : Integer.MAX_VALUE;
+            for(int i = 0; i < size; i++){
+                var node = q.poll();
+                if(lv % 2 == 0){
+                    if(node.val % 2 == 0 || node.val <= prev){
+                        return false;
+                    }
+                }else{
+                    if(node.val % 2 != 0 || node.val >= prev){
+                        return false;
+                    }
+                }
+                prev = node.val;
+                if(node.left != null){
+                    q.offer(node.left);
+                }
+                if(node.right != null){
+                    q.offer(node.right);
+                }
+            }
+            lv++;
+        }
+        return true;
+
     }
 }
