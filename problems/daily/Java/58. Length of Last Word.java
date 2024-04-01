21dedda411e544a2a0a91a0188a94282d8ef381f
Yukihito Nakaya <developer.nakaya@gmail.com>
2024-04-01 15:01:16 +0900

    58. Length of Last Word another solution added

--- problems/daily/Java/58. Length of Last Word.java
+++ problems/daily/Java/58. Length of Last Word.java
@@ -10,3 +10,19 @@ class Solution {
         return ans;
     }
 }
+
+//another solution
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        int ans = 0;
+        for(int i = s.length() - 1; i>=0; i--){
+            if(s.charAt(i) != ' ') ans++;
+            else if(ans > 0){
+                break;
+            }
+        }
+        return ans;
+    }
+}
