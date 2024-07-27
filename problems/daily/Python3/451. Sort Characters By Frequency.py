f4502b0f61038ce79c8521bc245fa182e4bd6e95
Yukihito Nakaya <developer.nakaya@gmail.com>
2024-07-27 14:52:59 +0900

    451. Sort Characters By Frequency バケツソート

--- problems/daily/Python3/451. Sort Characters By Frequency.py
+++ problems/daily/Python3/451. Sort Characters By Frequency.py
@@ -3,3 +3,20 @@ class Solution:
         count = defaultdict(int)
         for ch in s: count[ch] += 1
         return "".join([a[1]* a[0] for a in sorted([[count[ch],ch] for ch in count], reverse = True)])
+    
+#another solution
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        if not s: return s
+        count = collections.Counter(s)
+        maxq = max(count.values())
+        buckets = [[] for _ in range(maxq + 1)]
+
+        for ch, c in count.items():
+            buckets[c].append(ch)
+        res = []
+
+        for i in range(maxq, 0, -1):
+            for ch in buckets[i]:
+                res.append(ch * i)
+        return "".join(res)
