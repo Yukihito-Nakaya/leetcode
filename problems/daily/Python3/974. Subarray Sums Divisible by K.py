9205c54e748cac595fe49f49904c328e06f1f68f
Yukihito Nakaya <developer.nakaya@gmail.com>
2024-08-20 11:21:25 +0900

    560. Subarray Sum Equals K , another solution

--- problems/daily/Python3/974. Subarray Sums Divisible by K.py
+++ problems/daily/Python3/974. Subarray Sums Divisible by K.py
@@ -9,4 +9,19 @@ class Solution:
             ans += premap.get(mod, 0)
             premap[mod] = premap.get(mod, 0) + 1
         return ans
-    
+
+    #another solution
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        counter = defaultdict(int)
+        counter[0] = 1
+
+        nsum = 0
+        ans = 0
+
+        for i in nums:
+            nsum =(nsum + i) % k
+            ans += counter[nsum]
+            counter[nsum] = counter[nsum] + 1
+
+        return ans 
