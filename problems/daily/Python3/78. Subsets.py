4f2ff1225b88ae713adb966561ce599f93ef9e54
Yukihito Nakaya <developer.nakaya@gmail.com>
2024-10-11 18:32:56 +0900

    78. Subsets

--- problems/daily/Python3/78. Subsets.py
+++ problems/daily/Python3/78. Subsets.py
@@ -7,3 +7,24 @@ class Solution:
         ans = []
         dfs(0,[])
         return ans
+    
+# another solution
+from copy import deepcopy as cp
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        l = len(nums)
+        ans = []
+        sub = []
+
+        def rec(i) -> None:
+            if i  == l:
+                ans.append(cp(sub))
+                return
+            sub.append(nums[i])
+            rec(i + 1)
+            sub.pop()
+
+            rec(i + 1)
+            return
+        rec(0)
+        return ans
