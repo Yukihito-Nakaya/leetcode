f3c3fa120432a49e73a11da1dfd082f149b0891c
Yukihito Nakaya <developer.nakaya@gmail.com>
2024-10-25 13:50:18 +0900

    2487. Remove Nodes From Linked List

--- problems/daily/Python3/2487. Remove Nodes From Linked List.py
+++ problems/daily/Python3/2487. Remove Nodes From Linked List.py
@@ -20,3 +20,42 @@ class Solution:
             nxt = cur
         
         return cur
+    
+    #another solution
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        r_head = self.rLinkedList(head)
+        tent = ListNode(0)
+        tent.next = r_head
+        prev = tent
+        cur = r_head
+        maxv = cur.val
+
+        while cur:
+            if cur.val < maxv:
+                prev.next = cur.next
+            else:
+                maxv = cur.val
+                prev = cur
+            cur = cur.next
+        
+        return self.rLinkedList(tent.next)
+
+    
+    def rLinkedList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        cur = head
+
+        while cur:
+            next_node = cur.next
+            cur.next = prev
+            prev = cur
+            cur = next_node
+        
+        return prev
