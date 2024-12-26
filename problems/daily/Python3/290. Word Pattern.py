7313c6eaa00a35512569aebe2dca984531954158
Yukihito Nakaya <developer.nakaya@gmail.com>
2024-12-26 13:50:45 +0900

    290. Word Pattern

---
+++ problems/daily/Python3/290. Word Pattern.py
@@ -0,0 +1,5 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s = s.split()
+
+        return len(set(zip(pattern, s))) == len(set(pattern)) == len(set(s)) and len(pattern) == len(s)
