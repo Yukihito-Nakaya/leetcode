ba58116d3acaae716b2fcceb137d6556d7a14cc3
Yukihito-Nakaya <developer.nakaya@gmail.com>
2025-01-27 00:49:47 +0900

    961. N-Repeated Element in Size 2N Array

--- problems/daily/Ruby/961. N-Repeated Element in Size 2N Array.rb
+++ problems/daily/Ruby/961. N-Repeated Element in Size 2N Array.rb
@@ -2,4 +2,23 @@
 # @return {Integer}
 def repeated_n_times(nums)
     hash={}
+    scount = 1
+    ans = nums[0]
+    id = 1
+
+    while id < nums.length
+        num = nums[id]
+        if !nums_hash[num]
+            hash[num] = 1
+        else
+            count = hash[num] + 1
+            hash[num] = count
+            if count > scount
+                scount = count
+                ans = num
+            end
+        end
+        id = id + 1
+    end
+    ans
 end
