314964e96f0b22ea091c0569919bace64694ff2a
Yukihito-Nakaya <developer.nakaya@gmail.com>
2025-02-17 02:17:46 +0900

    1365. How Many Numbers Are Smaller Than the Current Number

--- problems/daily/Ruby/1365. How Many Numbers Are Smaller Than the Current Number.rb
+++ problems/daily/Ruby/1365. How Many Numbers Are Smaller Than the Current Number.rb
@@ -1,5 +1,14 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def smaller_numbers_than_current(nums)
-    
+    sorted = nums.sort
+    hash = Hash.new
+    sorted.each_with_index do |num,i|
+        next if hash[num]
+        hash[num] = i
+    end
+    nums.each_with_index do |num,i|
+        nums[i] = hash[num]
+    end
+    nums
 end
