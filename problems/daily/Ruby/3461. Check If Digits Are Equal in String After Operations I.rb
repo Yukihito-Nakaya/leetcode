e79a92afc3b991d1a95365d3bae85a7e1fde01a6
Yukihito-Nakaya <developer.nakaya@gmail.com>
2025-02-24 00:01:23 +0900

    3461. Check If Digits Are Equal in String After Operations I

--- problems/daily/Ruby/3461. Check If Digits Are Equal in String After Operations I.rb
+++ problems/daily/Ruby/3461. Check If Digits Are Equal in String After Operations I.rb
@@ -1,5 +1,15 @@
 # @param {String} s
 # @return {Boolean}
 def has_same_digits(s)
-    
+    ans = []
+    ans << s
+
+    while ans[-1].length != 2
+        res = ''
+        (0...ans[-1].length - 1).each do |i|
+            res += ((ans[-1][i].to_i + ans[-1][i+1].to_i)%10).to_s
+        end
+        ans << res
+    end
+    ans[-1][0] == ans[-1][1]
 end
