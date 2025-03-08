60f1b9001f14a6f44e01f4a3ef386440827f4754
Yukihito-Nakaya <developer.nakaya@gmail.com>
2025-03-08 19:39:37 +0900

    3452. Sum of Good Numbers

--- problems/daily/Ruby/3452. Sum of Good Numbers.rb
+++ problems/daily/Ruby/3452. Sum of Good Numbers.rb
@@ -2,5 +2,11 @@
 # @param {Integer} k
 # @return {Integer}
 def sum_of_good_numbers(nums, k)
-    
+    ans = 0
+    nums.each_with_index do |num, i|
+        next if i - k >= 0 && num <= nums[i-k]
+        next if i + k < nums.length && num <= nums[i+k]
+        ans += num
+    end
+    ans
 end
