72316c93de3a8cf755df0aa7b6124c5ae00c8e88
Yukihito-Nakaya <developer.nakaya@gmail.com>
2025-03-16 22:49:21 +0900

    2667. Create Hello World Function

--- problems/daily/JavaScript/2667. Create Hello World Function.js
+++ problems/daily/JavaScript/2667. Create Hello World Function.js
@@ -2,10 +2,10 @@
  * @return {Function}
  */
 var createHelloWorld = function() {
-    
-    return function(...args) {
-        
-    }
+    return () => "Hello World";
+    // return function() {
+    //     return "Hello World"
+    // }
 };
 
 /**
